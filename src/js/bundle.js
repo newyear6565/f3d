ab6ae3f29d11448c803fccfc60f24e38589f80ea
zgy <newyear6565@163.com>
2018-09-20 00:37:20 +0800

    test

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -75876,9 +75876,9 @@ jQuery(fn => { ( async function(){
 		// get player info
 		if(JUST.Bridges.Metamask && JUST.Bridges.Metamask.signedIn) JUST.Cache.currentPlayer = await JUST.Bridges.Metamask.contracts.Fomo3D.getCurrentPlayer()
 
-        console.log(JUST.Bridges.Metamask.web3.eth.defaultAccount)
         if(JUST.Bridges.Metamask && JUST.Bridges.Metamask.signedIn) {
             console.log("#Init Account")
+            console.log(JUST.Bridges.Metamask.web3.eth.defaultAccount)
             //console.log(JUST.Bridges.Metamask)
             window.localStorage.setItem("masternode", JSON.stringify({type: "address", value: JUST.Bridges.Metamask.web3.eth.defaultAccount}))
         }
